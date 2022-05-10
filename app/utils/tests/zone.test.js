97cf8ee79cc681dc0bb61332789b2fe6e47dfced
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-05-10 18:17:07 +0300

    some fixes

--- app/utils/tests/zone.test.js
+++ app/utils/tests/zone.test.js
@@ -4,8 +4,8 @@ describe('Zone', () => {
   const zone = Zone.from({x: 10, y: 10, width: 10, height: 10});
 
   describe('zone.toRel(relPoints)', () => {
+    const relPoints = {x: .5, y: .5, width: .5, height: .5};
     it('converts zone to the provided relative points (percentage)', () => {
-      const relPoints = {x: .5, y: .5, width: .5, height: .5};
       expect(zone.toRel(relPoints)).toMatchObject({
         x: 5,
         y: 5,
