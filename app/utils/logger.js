38e15828c50e2057ddd806087399e863049be4f2
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-05-12 20:45:46 +0300

    refactor logger

--- app/utils/logger.js
+++ app/utils/logger.js
@@ -1,42 +1,26 @@
-class Log {
-  constructor(win) {
-    this.win = win;
-  }
+const getCurrentTime = require('./getCurrentTime.js');
 
-  send(text, type = 'black') {
-    const date = getCurrentTime();
-    text = `[${date.hr}:${date.min}:${date.sec}] ${text}`;
-    this.win.webContents.send('log-data', {text, type});
-  }
+const createLog = (sendToWindow) => {
+  return {
+    send(text, type = 'black') {
+      const date = getCurrentTime();
+      text = `[${date.hr}:${date.min}:${date.sec}] ${text}`;
+      sendToWindow({text, type});
+    },
 
-  ok(text) {
-    this.send(text, 'green');
-  }
+    ok(text) {
+      this.send(text, 'green');
+    },
 
-  warn(text) {
-    this.send(text, 'orange');
-  }
+    warn(text) {
+      this.send(text, 'orange');
+    },
 
-  err(text) {
-    this.send(text, 'red')
-  }
-
-  static setTo(win) {
-    return new Log(win);
+    err(text) {
+      this.send(text, 'red')
+    }
   }
 }
 
-const getCurrentTime = () => {
-  let date = new Date();
-  let times = {hr: date.getHours(),min: date.getMinutes(), sec: date.getSeconds()};
-  for(let time of Object.keys(times)) {
-    times[time] = times[time].toString();
-    if(times[time].length < 2) {
-      times[time] = times[time].padStart(2, `0`);
-    }
-  }
 
-  return times;
-};
-
-module.exports = Log;
+module.exports = createLog;
