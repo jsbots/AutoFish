d5a498e6425d4d694642f87b52f56c7315f4d545
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-05-13 19:08:23 +0300

    Settings fixes

--- app/renderer.js
+++ app/renderer.js
@@ -12,25 +12,25 @@ function elt(type, props, ...children) {
 
 const renderGameNames = (configName) => {
   const gameNames = ['Retail&classic', 'MoP', 'Cataclysm', 'WoTLK', 'TBC', 'Vanilla'];
-  return elt('select', {name: 'name'},
+  return elt('select', {name: 'name', className: 'option'},
          ...gameNames.map((name) => elt('option', {selected: name == configName}, name)));
 };
 const renderTimer = (configTimer) => {
-  return elt('input', {type: 'number', min: '1', value: configTimer, name: 'timer'}, `(min)`);
+  return elt('input', {type: 'number', min: '1', className: 'option', value: configTimer, name: 'timer'}, `(min)`);
 };
 const renderControl = (configControl) => {
   const select = configControl == 'Hardware' ? true : false;
-  return elt('select', {name: 'control'},
+  return elt('select', {name: 'control', className: 'option'},
          elt('option', {selected: select}, 'Hardware'),
          elt('option', {selected: !select}, 'Virtual'));
 };
 
 const renderAutoLoot = (configAutoLoot) => {
-  return elt('input', {type: 'checkbox', checked: !!configAutoLoot, name: 'autoLoot'})
+  return elt('input', {type: 'checkbox', className: 'option', checked: !!configAutoLoot, name: 'autoLoot'})
 };
 
 const renderMultiple = (configMultiple) => {
-  return elt('input', {type: 'checkbox', checked: !!configMultiple, name: 'multiple'});
+  return elt('input', {type: 'checkbox', className: 'option', checked: !!configMultiple, name: 'multiple'});
 }
 
 class Settings {
@@ -39,11 +39,15 @@ class Settings {
     const {name, timer, control, autoLoot, multiple} = config;
 
     this.dom = elt('section', {className: 'settings'},
-               elt('label', null, `Game:`, renderGameNames(name)),
-               elt('label', null, `Timer:`, renderTimer(timer)),
-               elt('label', null, `Control:`,renderControl(control)),
-               elt('label', null, `AutoLoot:`, renderAutoLoot(autoLoot)),
-               elt('label', null, `Multiple windows:`, renderMultiple(multiple)));
+               elt('div', null,
+                elt('label', null, `Game:`, renderGameNames(name)),
+                elt('label', null, `Timer:`, renderTimer(timer)),
+                elt('label', null, `Control:`,renderControl(control))
+               ),
+               elt('div', null,
+                elt('label', null, `AutoLoot:`, renderAutoLoot(autoLoot)),
+                elt('label', null, `Multiple windows:`, renderMultiple(multiple)))
+              );
 
     this.dom.addEventListener('change', (event) => {
       const value = event.target.value;
