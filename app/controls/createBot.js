48c5716194e2121da6885d6dae9e04f8ea544568
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-05-14 17:57:36 +0300

    Multiple windows support + refactor

---
+++ app/controls/createBot.js
@@ -0,0 +1,55 @@
+const keysender = require("keysender");
+
+const createGame = require("./createGame.js");
+const { createLog, bindLogToID } = require("../utils/logger.js");
+const createWinSwitch = require('../utils/createWinSwitch.js');
+
+const bot = require("../fishingBot/bot.js");
+const runBot = require("../fishingBot/runBot.js");
+
+const Zone = require("../utils/zone.js");
+const State = require("../fishingBot/state.js");
+const EventLine = require('../utils/eventLine.js');
+
+const createBot = () => {
+  let state;
+  return {
+    async startBot(win, config, onError) {
+      const log = createLog((data) => {
+        win.webContents.send("log-data", data);
+      });
+
+      log.send("Starting the bot...");
+      const games = createGame(keysender).findWindows(config.game);
+      if (games.length == 0) {
+        log.err(`Can't find any window of the game!`);
+        onError();
+      } else {
+        log.ok(`Found ${games.length} window${games.length > 1 ? `s` : ``} of the game!`)
+      }
+
+      const winSwitch = createWinSwitch(new EventLine());
+      state = new State();
+
+      win.blur();
+
+      let id = 0;
+      for (const game of games) {
+        const zone = Zone.from(game.workwindow.getView());
+        runBot(bot(game, zone, config.patch.mop), bindLogToID(log, ++id), state, winSwitch)
+          .then((stats) => log.ok(stats))
+          .catch((error) => {
+            log.err(`${(error.message, error.stack)}`);
+            onError();
+          });
+      }
+    },
+    stopBot() {
+      if (state) {
+        state.status = "stop";
+      }
+    },
+  };
+};
+
+module.exports = createBot;
