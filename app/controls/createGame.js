4e227a24965be13acb2bfa11fa8e206ab6faa658
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-05-14 17:57:47 +0300

    Multiple windows support

--- app/controls/createGame.js
+++ app/controls/createGame.js
@@ -1,22 +1,13 @@
-class GameError extends Error{
-  constructor() {
-    super();
-    this.message = `Can't find the window of the game!`
-  }
-};
-
 const createGame = ({Virtual, Hardware, getAllWindows}) => {
   return {
-    findGame({names, classNames, controlType}) {
-      const win = getAllWindows().find(({title, className}) =>  names.includes(title) && classNames.includes(className));
-      if(win) {
+    findWindows({names, classNames, controlType}) {
+      const wins = getAllWindows().filter(({title, className}) =>  names.includes(title) && classNames.includes(className));
+      if(wins.length > 0) {
         if(controlType == 'virtual') {
-          return new Virtual(win.handle);
+          return wins.map((win) => new Virtual(win.handle));
         } else {
-          return new Hardware(win.handle);
+          return wins.map((win) => new Hardware(win.handle));
         }
-      } else {
-        throw new GameError();
       }
     }
   }
