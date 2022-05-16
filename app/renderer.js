1193286e7bf3c81162938705b15a29cc1d75373f
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-05-16 19:48:14 +0300

    settings and instruction separation

--- app/renderer.js
+++ app/renderer.js
@@ -3,15 +3,15 @@ const { ipcRenderer } = require("electron");
 const Settings = require('./ui/settings.js');
 const StartButton = require('./ui/startButton.js');
 const AutoFish = require('./ui/autoFish.js');
-const getSettings = require('./ui/getSettings.js');
 
 const runApp = async () => {
-  const config = await getSettings();
-  let autoFish = new AutoFish(config, new Settings(config), new StartButton);
+  const {settings, instructions} = await ipcRenderer.invoke('get-settings');
+  console.log(settings);
+  let autoFish = new AutoFish(instructions,
+                              new Settings(settings),
+                              new StartButton);
   document.querySelector('.AutoFish').append(autoFish.dom);
-  ipcRenderer.on('log-data', (event, data) => {
-    autoFish.log(data);
-  })
 };
+
 
 runApp();
