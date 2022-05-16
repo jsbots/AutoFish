8bd7a5019967143cd349c2170b5b45f0a6bfedca
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-05-16 19:46:19 +0300

    syncState for control and autoLoot

--- app/ui/settings.js
+++ app/ui/settings.js
@@ -1,52 +1,90 @@
 const elt = require('./utils/elt.js');
 
 const renderGameNames = (configName) => {
-  const gameNames = ['Retail&classic', 'MoP', 'Cataclysm', 'WoTLK', 'TBC', 'Vanilla'];
-  return elt('select', {name: 'name', className: 'option'},
+  const gameNames = ['Retail&Classic', 'MoP', 'Cataclysm', 'WoTLK', 'TBC', 'Vanilla'];
+  return elt('select', {name: 'game', className: 'option'},
          ...gameNames.map((name) => elt('option', {selected: name == configName}, name)));
 };
 
 const renderTimer = (configTimer) => {
-  return elt('input', {type: 'number', min: '1', className: 'option', value: configTimer, name: 'timer'}, `(min)`);
+  return elt('input', {type: 'number', min: '1', value: configTimer, name: 'timer'}, `(min)`);
 };
 
-const renderControl = (configControl) => {
+const wrapInLabel = (name, inner, hint) => {
+  return elt('label', null, name,
+         elt('div', {className: 'option'},
+          inner,
+         elt('img', {src: './img/hint.png', className:'option_hint', title: hint})));
+}
+
+const createControl = (configControl) => {
   const select = configControl == 'Hardware' ? true : false;
-  return elt('select', {name: 'control', className: 'option'},
-         elt('option', {selected: select}, 'Hardware'),
-         elt('option', {selected: !select}, 'Virtual'));
+  const dom = elt('select', {name: 'control', className: 'option'},
+              elt('option', {selected: select}, 'Hardware'),
+              elt('option', {selected: !select}, 'Virtual'));
+  return {
+    dom,
+    syncState(game) {
+      if(game != 'Retail&Classic') {
+        dom.children[0].selected = true;
+        dom.children[1].disabled = true;
+      } else {
+        dom.children[1].removeAttribute('disabled');
+      }
+    }
+  }
 };
-
-const renderAutoLoot = (configAutoLoot) => {
-  return elt('input', {type: 'checkbox', className: 'option', checked: !!configAutoLoot, name: 'autoLoot'})
+const createAutoLoot = (configAutoLoot) => {
+  const dom = elt('input', {type: 'checkbox', className: 'option', checked: !!configAutoLoot, name: 'autoLoot'});
+  return  {
+    dom,
+    syncState(game) {
+      if(game == 'Vanilla') {
+        dom.checked = true;
+        dom.setAttribute('disabled', true);
+      } else {
+        dom.removeAttribute('disabled');
+      }
+    }
+  }
 };
 
 const renderMultiple = (configMultiple) => {
   return elt('input', {type: 'checkbox', className: 'option', checked: !!configMultiple, name: 'multiple'});
 }
 
+
 class Settings {
   constructor(config) {
     this.config = config;
-    const {name, timer, control, autoLoot, multiple} = config;
-
+    const {name, timer, control, autoLoot, multiple, fishingKey} = config;
+    this.options = {
+      control: createControl(control),
+      autoLoot: createAutoLoot(autoLoot)
+    }
     this.dom = elt('section', {className: 'settings'},
-               elt('div', null,
-                elt('label', null, `Game:`, renderGameNames(name)),
-                elt('label', null, `Timer:`, renderTimer(timer)),
-                elt('label', null, `Control:`,renderControl(control))
-               ),
-               elt('div', null,
-                elt('label', null, `AutoLoot:`, renderAutoLoot(autoLoot)),
-                elt('label', null, `Multiple windows:`, renderMultiple(multiple)))
+               elt('div', {className: 'settings_section'},
+                wrapInLabel('Game:', renderGameNames(name), `Choose the patch you want the bot to work on.`),
+                wrapInLabel('Timer: ', renderTimer(timer), 'The bot will work for the provided period of time. If 0 it will never stop.'),
+                wrapInLabel('Control: ', this.options.control.dom, `Hardware mode will open the window of the game and will directly use you mouse and keyboard. You won't be able to use the computer while the bot is working.`)
+                ),
+               elt('div', {className: 'settings_section'},
+                wrapInLabel('AutoLoot: ', this.options.autoLoot.dom, `Use shift + click instead of Auto Loot. Check this option if you don't want to turn on Auto Loot option in the game, or if there's no such an option (like in Vanilla)`),
+                wrapInLabel('Multiple windows: ', renderMultiple(multiple), `The bot will look for all the windows named "World of Warcraft" and start fishing on them.`)
+              )
               );
 
     this.dom.addEventListener('change', (event) => {
       const value = event.target.value;
       const name = event.target.name;
       config[name] = value;
-      if(name == 'name' && this.onGameChange)
+      if(name == 'game') {
         this.onGameChange(value);
+        for(let option of Object.keys(this.options)) {
+          this.options[option].syncState(value);
+        }
+      }
+
       })
   }
 
