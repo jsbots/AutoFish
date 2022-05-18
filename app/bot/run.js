fbcce62013992a8649ee32a467978f895c3135f8
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-05-18 12:47:48 +0300

    log.ok(showStats()) + removed winSwitch

--- app/bot/run.js
+++ app/bot/run.js
@@ -1,14 +1,10 @@
-const runBot = async (bot, log, state, winSwitch) => {
-  const {castFishing, findBobber, checkBobber, hookBobber, gameWindow} = bot;
+const runBot = async (bot, log, state) => {
+  const {castFishing, findBobber, checkBobber, hookBobber} = bot;
   findBobber.attempts = 0;
   do {
       log.send(`Casting fishing...`);
 
-      await winSwitch.reg(() => {
-        gameWindow.setForeground();
-      })
       await castFishing(state);
-      winSwitch.unreg();
 
       log.send(`Looking for the bobber in...`);
       let bobber = findBobber();
@@ -25,13 +21,7 @@ const runBot = async (bot, log, state, winSwitch) => {
 
       log.send(`Checking the hook...`);
       if(bobber = await checkBobber(bobber, state)) {
-
-        await winSwitch.reg(() => {
-          gameWindow.setForeground();
-        })
         let isHooked = await hookBobber(bobber);
-        winSwitch.unreg();
-
         if(isHooked) {
           state.caught(() => log.ok(`Caught the fish!`));
           continue;
@@ -40,7 +30,7 @@ const runBot = async (bot, log, state, winSwitch) => {
 
       state.miss(() => log.warn(`Missed the fish!`));
   } while(state.status == 'working');
-  return state.showStats();
+log.ok(state.showStats());
 }
 
 module.exports = runBot;
