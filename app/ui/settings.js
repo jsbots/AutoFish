a030f4a00f70b7870baf0f08317adbe56a3e7ff6
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-05-19 20:23:22 +0300

    removed control option

--- app/ui/settings.js
+++ app/ui/settings.js
@@ -17,23 +17,6 @@ const wrapInLabel = (name, inner, hint) => {
          elt('img', {src: './img/hint.png', className:'option_hint', title: hint})));
 }
 
-const createControl = (configControl) => {
-  const select = configControl == 'Hardware' ? true : false;
-  const dom = elt('select', {name: 'control', className: 'option'},
-              elt('option', {selected: select}, 'Hardware'),
-              elt('option', {selected: !select}, 'Virtual'));
-  return {
-    dom,
-    syncState(game) {
-      if(game != 'Retail&Classic') {
-        dom.children[0].selected = true;
-        dom.children[1].disabled = true;
-      } else {
-        dom.children[1].removeAttribute('disabled');
-      }
-    }
-  }
-};
 const createShiftClick = (configShiftClick) => {
   const dom = elt('input', {type: 'checkbox', className: 'option', checked: !!configShiftClick, name: 'shiftClick'});
   let saved = null;
@@ -63,20 +46,18 @@ const renderLikeHuman = (configLikeHuman) => {
 class Settings {
   constructor(config) {
     this.config = config;
-    const {game, timer, control, shiftClick, likeHuman, fishingKey} = config;
+    const {game, timer, shiftClick, likeHuman, fishingKey} = config;
 
     this.options = {
-      control: createControl(control),
       shiftClick: createShiftClick(shiftClick)
     }
     this.dom = elt('section', {className: 'settings'},
                elt('div', {className: 'settings_section'},
                 wrapInLabel('Game:', renderGameNames(game), `Choose the patch you want the bot to work on.`),
                 wrapInLabel('Timer: ', renderTimer(timer), `The bot will work for the given period of time. If it's 0 or nothing at all, it will never stop.`),
-                wrapInLabel('Control: ', this.options.control.dom, `Hardware mode will open the window of the game and will directly use your mouse and keyboard. You should not use them while the bot is working or the bot will not work properly.`)
                 ),
                elt('div', {className: 'settings_section'},
-                wrapInLabel('Use shift + click: ', this.options.shiftClick.dom, `Use shift + click instead of Auto Loot. Check this option if you don't want to turn on Auto Loot option in the game, or if there's no such an option (like in Vanilla). Your "Loot key" in the game should be assign to shift.`),
+                wrapInLabel('Use shift + click: ', this.options.shiftClick.dom, `Use shift + click instead of Auto Loot. Check this option if you don't want to turn on Auto Loot option in the game. Your "Loot key" in the game should be assign to shift.`),
                 wrapInLabel('Like a human: ', renderLikeHuman(likeHuman), `The bot will move your mouse in a human way: random speed and with a slight random curvature. Otherwise it will move the mouse instantly, which might be a better option if you use many windows. `)
               )
               );
