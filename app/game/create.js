b99cb06d5ba4163a5ad5785633a6257936acf751
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-05-19 20:24:42 +0300

    removed Virtual option

--- app/game/create.js
+++ app/game/create.js
@@ -1,13 +1,9 @@
 const createGame = ({Virtual, Hardware, getAllWindows}) => {
   return {
-    findWindows({names, classNames, controlType}) {
+    findWindows({names, classNames}, control) {
       const wins = getAllWindows().filter(({title, className}) =>  names.includes(title) && classNames.includes(className));
       if(wins.length > 0) {
-        if(controlType == 'virtual') {
-          return wins.map((win) => new Virtual(win.handle));
-        } else {
           return wins.map((win) => new Hardware(win.handle));
-        }
       }
     }
   }
