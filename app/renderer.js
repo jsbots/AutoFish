6146900b7c38e7094c24a05ba81fc756a8503000
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-05-20 17:38:31 +0300

    prettier

--- app/renderer.js
+++ app/renderer.js
@@ -1,16 +1,17 @@
 const { ipcRenderer } = require("electron");
 
-const Settings = require('./ui/settings.js');
-const StartButton = require('./ui/startButton.js');
-const AutoFish = require('./ui/autoFish.js');
+const Settings = require("./ui/settings.js");
+const StartButton = require("./ui/startButton.js");
+const AutoFish = require("./ui/autoFish.js");
 
 const runApp = async () => {
-  const {settings, instructions} = await ipcRenderer.invoke('get-settings');
-  let autoFish = new AutoFish(instructions,
-                              new Settings(settings),
-                              new StartButton);
-  document.querySelector('.AutoFish').append(autoFish.dom);
+  const { settings, instructions } = await ipcRenderer.invoke("get-settings");
+  let autoFish = new AutoFish(
+    instructions,
+    new Settings(settings),
+    new StartButton()
+  );
+  document.querySelector(".AutoFish").append(autoFish.dom);
 };
-
 
 runApp();
