b3c3e2faf41f68b786257b04fd241e798ef8b436
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-05-20 17:11:11 +0300

    prettier

--- app/bot/run.js
+++ app/bot/run.js
@@ -1,48 +1,47 @@
-const Stats = require('./stats.js');
-const { convertMs } = require('../utils/time.js');
+const Stats = require("./stats.js");
+const { convertMs } = require("../utils/time.js");
 
 const runBot = async (bot, log, state) => {
-  const {castFishing, findBobber, checkBobber, hookBobber} = bot;
+  const { castFishing, findBobber, checkBobber, hookBobber } = bot;
   const stats = new Stats();
   findBobber.attempts = 0;
 
   do {
-      log.send(`Casting fishing...`);
+    log.send(`Casting fishing...`);
+    await castFishing(state);
+    log.send(`Looking for the bobber in...`);
+    let bobber = findBobber();
+    if (bobber) {
+      log.ok(`Found the bobber!`);
+      findBobber.attempts = 0;
+    } else {
+      log.err(`Can't find the bobber, will try again.`);
+      if (++findBobber.attempts == 3) {
+        throw new Error(
+          `Have tried 3 attempts to find the bobber and failed: this place isn't good for fishing`
+        );
+      }
+      continue;
+    }
 
-      await castFishing(state);
-
-      log.send(`Looking for the bobber in...`);
-      let bobber = findBobber();
-      if(bobber) {
-        log.ok(`Found the bobber!`);
-        findBobber.attempts = 0;
-      } else {
-        log.err(`Can't find the bobber, will try again.`);
-        if(++findBobber.attempts == 3) {
-          throw new Error(`Have tried 3 attempts to find the bobber and failed: this place isn't good for fishing`);
-        }
+    log.send(`Checking the hook...`);
+    if ((bobber = await checkBobber(bobber, state))) {
+      let isHooked = await hookBobber(bobber);
+      if (isHooked) {
+        stats.caught++;
+        log.ok(`Caught the fish!`);
         continue;
       }
+    }
 
-      log.send(`Checking the hook...`);
-      if(bobber = await checkBobber(bobber, state)) {
-        let isHooked = await hookBobber(bobber);
-        if(isHooked) {
-          stats.caught++;
-          log.ok(`Caught the fish!`);
-          continue;
-        }
-      }
+    if (state.status == "working") {
+      stats.miss++;
+      log.warn(`Missed the fish!`);
+    }
+  } while (state.status == "working");
 
-      if(state.status == 'working') {
-        stats.miss++;
-        log.warn(`Missed the fish!`);
-      }
-  } while(state.status == 'working');
-
-log.ok(stats.showStats());
-log.ok(`Time Passed: ${convertMs(Date.now() - state.startTime)}`);
-}
-
+  log.ok(stats.showStats());
+  log.ok(`Time Passed: ${convertMs(Date.now() - state.startTime)}`);
+};
 
 module.exports = runBot;
