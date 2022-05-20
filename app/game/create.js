759effb38aab540760fd6a3881fb956e73216ba2
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-05-20 17:40:35 +0300

    removed virtual and control options

--- app/game/create.js
+++ app/game/create.js
@@ -1,6 +1,6 @@
-const createGame = ({Virtual, Hardware, getAllWindows}) => {
+const createGame = ({Hardware, getAllWindows}) => {
   return {
-    findWindows({names, classNames}, control) {
+    findWindows({names, classNames}) {
       const wins = getAllWindows().filter(({title, className}) =>  names.includes(title) && classNames.includes(className));
       if(wins.length > 0) {
           return wins.map((win) => new Hardware(win.handle));
