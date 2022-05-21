55465b9d9224cb9a9f03a200fd427c228b88fc58
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-05-21 10:33:12 +0300

    random generated name of the window

--- app/main.js
+++ app/main.js
@@ -76,6 +76,14 @@ function handleSquirrelEvent() {
   }
 }
 
+const generateName = (size) => {
+  let alphabet = `AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890`;
+  return new Array(size)
+  .fill(true)
+  .map(slot => alphabet[Math.floor(Math.random() * alphabet.length)])
+  .join(``);
+};
+
 /* Electron */
 
 let win;
@@ -84,6 +92,7 @@ let powerBlocker = powerSaveBlocker.start("prevent-display-sleep");
 
 function createWindow() {
   win = new BrowserWindow({
+    title: generateName(10),
     width: 325,
     height: 650,
     show: false,
