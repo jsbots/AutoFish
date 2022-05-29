fba2015878197fc6048c7ce3939823e3f8924254
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-05-29 21:27:35 +0300

    settings refactor

--- app/renderer.js
+++ app/renderer.js
@@ -1,6 +1,7 @@
 const { ipcRenderer } = require("electron");
 
 const Settings = require("./ui/settings.js");
+const renderSettings = require("./ui/renderSettings.js");
 const StartButton = require("./ui/startButton.js");
 const AutoFish = require("./ui/autoFish.js");
 
@@ -8,7 +9,7 @@ const runApp = async () => {
   const { settings, instructions } = await ipcRenderer.invoke("get-settings");
   let autoFish = new AutoFish(
     instructions,
-    new Settings(settings),
+    new Settings(settings, renderSettings),
     new StartButton()
   );
   document.body.append(autoFish.dom);
