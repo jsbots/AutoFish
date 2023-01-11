6e402b7a28d8bad4f3f3ac897abe9fd6e14f3c8e
olesgeras <26541518+olesgeras@users.noreply.github.com>
2023-01-11 13:39:48 +0200

    1.14.3

--- app/ui/autoFish.js
+++ app/ui/autoFish.js
@@ -51,7 +51,7 @@ class AutoFish {
     const footer = elt(`p`, { className: "version" }, versionNode, donateLink, premiumIcon);
 
     ipcRenderer.on("set-version", (event, version) => {
-      versionNode.textContent = `ver. 1.14.2 Public | `;
+      versionNode.textContent = `ver. 1.14.3 Public | `;
     });
     this.settings.regOnChange((config) => {
       ipcRenderer.send("save-settings", config);
