2ada3d1c78fd3db262711c4841342192bb391fe5
jsbots <26541518+olesgeras@users.noreply.github.com>
2023-06-26 16:24:53 +0300

    updated license warning

--- app/main.js
+++ app/main.js
@@ -124,12 +124,25 @@ const createWindow = async () => {
     if(settings.initial) {
       showWarning(win, `The shortcut to AutoFish was created on you desktop`);
 
-      if(showChoiceWarning(win, `The software is provided "as is" and the author disclaims all warranties
-with regard to this software. In no event shall the author be liable for
-any special, direct, indirect, or consequential damages or any damages
-whatsoever resulting from loss of use or data, whether in an
-action of contract, negligence or other tortious action, arising out of
-or in connection with the use or performance of this software.`, `Disclaimer`, `I agree`, `I don't agree`)) {
+      if(showChoiceWarning(win, `Copyright (c) 2023 jsbots
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`,
+ `MIT License`, `I agree`, `I don't agree`)) {
         app.quit();
       } else {
         settings.initial = false;
