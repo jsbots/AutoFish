da0e09cb1f099665577c5704ddf30c16a2c82b69
jsbots <26541518+olesgeras@users.noreply.github.com>
2023-10-11 11:02:33 +0300

    warning text fixes

--- app/main.js
+++ app/main.js
@@ -122,7 +122,7 @@ const createWindow = async () => {
     });
     const settings = getJson("./config/settings.json");
     if(settings.initial) {
-      showWarning(win, `The shortcut to AutoFish was created on you desktop`);
+      showWarning(win, `The shortcut to AutoFish was created on your desktop`);
 
       if(showChoiceWarning(win, `Copyright (c) 2023 jsbots
 
@@ -142,7 +142,7 @@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`,
- `MIT License`, `I accept`, `I don't accept`)) {
+ `MIT License`, `Accept`, `Decline`)) {
         app.quit();
       } else {
         settings.initial = false;
