76683a5541e4a1ac8c88292723c4cc244f08bd6c
jsbots <26541518+olesgeras@users.noreply.github.com>
2023-10-16 16:30:24 +0300

    ver. bump 2.2.4

--- app/ui/autoFish.js
+++ app/ui/autoFish.js
@@ -39,7 +39,7 @@ class AutoFish {
     const footer = elt(`p`, { className: "version" }, versionNode, donateLink, premiumIcon);
 
     ipcRenderer.on("set-version", (event, version) => {
-      versionNode.textContent = `ver. 2.2.3 Public | `;
+      versionNode.textContent = `ver. 2.2.4 Public | `;
     });
     this.settings.regOnChange((config) => {
       ipcRenderer.send("save-settings", config);
