b4d5e18707a450715637eda9fe22b6830d8dff16
jsbots <26541518+olesgeras@users.noreply.github.com>
2023-11-02 14:05:08 +0200

    fix: license fix

--- app/main.js
+++ app/main.js
@@ -128,7 +128,9 @@ let log;
     if(settings.initial) {
       showWarning(win, `The shortcut to AutoFish was created on your desktop`);
 
-      if(showChoiceWarning(win, `This project was solely developed for educational purposes, aiming to explore the feasibility of creating a functional gaming bot using web-development technologies only. The software provided should never be used with real-life applications, games and servers.
+      if(showChoiceWarning(win, `This project was developed for educational purposes, aiming to explore the feasibility of creating a functional gaming bot using web-development technologies only. The software provided should never be used with real-life applications, games and servers outside private "sandbox".
+
+This project was developed for people with limited mobility, as they may experience difficulty with motoric actions, such as those required by the fishing mechanic in games.
 
 You assume full responsibility for any outcomes that may arise from using this software. It's essential to acknowledge that this software is not designed to be "undetectable" in any way, nor was it ever intended for such purposes as stated above. As a result, no guarantees or assurances can be made regarding the functionality or outcomes of the bot.
 
