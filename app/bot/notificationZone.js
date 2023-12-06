c781b63235abf9a98d7c18d199fa9e22afc39c92
jsbots <26541518+olesgeras@users.noreply.github.com>
2023-12-06 11:37:14 +0200

    fix: notification warning color decreased

--- app/bot/notificationZone.js
+++ app/bot/notificationZone.js
@@ -3,7 +3,7 @@ const createRgb = require("../utils/rgb.js");
 const createNotificationZone = ({ getDataFrom, zone }) => {
   const notifications = {
     isWarning: ([r, g, b]) => r - b > 240 && g - b > 240,
-    isError: ([r, g, b]) => r - g > 220 && r - b > 220
+    isError: ([r, g, b]) => r - g > 200 && r - b > 200
   }
 
   return {
