4916e77ec16a26155a1942e02436035351eeca54
jsbots <26541518+olesgeras@users.noreply.github.com>
2023-12-22 15:24:45 +0200

    ver: 2.5.1

--- app/ui/autoFish.js
+++ app/ui/autoFish.js
@@ -39,7 +39,7 @@ class AutoFish {
     const footer = elt(`p`, { className: "version" }, versionNode, donateLink, premiumIcon);
 
     ipcRenderer.on("set-version", (event, version) => {
-      versionNode.textContent = `ver. 2.5.0 Public | `;
+      versionNode.textContent = `ver. 2.5.1 Public | `;
     });
     this.settings.regOnChange((config) => {
       ipcRenderer.send("save-settings", config);
