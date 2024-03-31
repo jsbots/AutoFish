b1296e968005c2155c0e2b3c42bdaec89d55f1b5
jsbots <26541518+olesgeras@users.noreply.github.com>
2024-03-31 17:35:13 +0300

    ver. 2.8.1

--- app/ui/autoFish.js
+++ app/ui/autoFish.js
@@ -44,7 +44,7 @@ class AutoFish {
     const footer = elt(`p`, { className: "version" }, versionNode, donateLink, premiumIcon);
 
     ipcRenderer.on("set-version", (event, version) => {
-      versionNode.textContent = `ver. 2.8.0 Public | `;
+      versionNode.textContent = `ver. 2.8.1 Public | `;
     });
 
     ipcRenderer.on('start-by-fishing-key', () => {
